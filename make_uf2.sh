4b264d2a1ded0c92bff152c802654f0e439d11a1
Erik Tollerud <erik.tollerud@gmail.com>
2023-02-23 22:56:31 -0500

    build and cp

--- make_uf2.sh
+++ make_uf2.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-cargo objcopy --release --bin nrf_accelhid -- -O binary toflash.bin
-uf2conv toflash.bin --base 0x26000 --family 0xADA52840 --output toflash.uf2
-rm toflash.bin
-#cp toflash.uf2 /run/media/erik/ITSYBOOT/
+cargo objcopy --release --bin nrf_accelhid -- -O binary target/toflash.bin
+uf2conv target/toflash.bin --base 0x26000 --family 0xADA52840 --output target/toflash.uf2
+rm target/toflash.bin
+cp target/toflash.uf2 /run/media/erik/ITSY840BOOT/
