f370c96086caa7478549e3fa6a54332b0a5b7985
Erik Tollerud <erik.tollerud@gmail.com>
2023-02-23 22:54:25 -0500

    clean up

--- make_uf2.sh
+++ make_uf2.sh
@@ -4,4 +4,5 @@ set -o errexit
 
 cargo objcopy --release --bin nrf_accelhid -- -O binary toflash.bin
 uf2conv toflash.bin --base 0x26000 --family 0xADA52840 --output toflash.uf2
+rm toflash.bin
 #cp toflash.uf2 /run/media/erik/ITSYBOOT/
