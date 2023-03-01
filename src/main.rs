695599500dee8a0df6152f88814071e03f64c757
Erik Tollerud <erik.tollerud@gmail.com>
2023-03-01 01:36:52 -0500

    flash writer working

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,24 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use defmt::*;
 use embassy_executor::Spawner;
 //use embassy_time::{Duration, Timer};
 
 use embassy_nrf;
 use embassy_nrf::gpio::*;
-use embassy_nrf::{interrupt, uarte, config};
+use embassy_nrf::{interrupt, uarte, config, qspi};
 
+use numtoa::NumToA;
 
-
-use defmt_rtt as _; // global logger
 use panic_persist;
 
+const FLASH_PAGE_SIZE: usize = 4096;
+// Workaround for alignment requirements in flash writing
+#[repr(C, align(4))]
+struct AlignedBuf([u8; FLASH_PAGE_SIZE]);
+
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     let mut init_config = config::Config::default();
     init_config.lfclk_source = config::LfclkSource::ExternalXtal;
     let p = embassy_nrf::init(init_config);
@@ -28,11 +31,11 @@ async fn main(spawner: Spawner) {
     let mut uart = uarte::Uarte::new(p.UARTE0, uart_irq, p.P0_25, p.P0_24, uart_config);
 
     if let Some(msg) = panic_persist::get_panic_message_bytes() {
-        unwrap!(uart.write(b"Panic message on boot!:\r\n").await);
-        unwrap!(uart.write(msg).await);
-        unwrap!(uart.write(b"\r\n").await);
+        uart.write(b"Panic message on boot!:\r\n").await.unwrap();
+        uart.write(msg).await.unwrap();
+        uart.write(b"\r\n").await.unwrap();
     } else {
-        unwrap!(uart.write(b"No panic error message on boot.\r\n").await);
+        uart.write(b"No panic error message on boot.\r\n").await.unwrap();
     }
 
 
@@ -42,6 +45,76 @@ async fn main(spawner: Spawner) {
     let mut redled = Output::new(p.P0_06, Level::High, OutputDrive::Standard);
 
     // action
+
+    let mut config = qspi::Config::default();
+    config.read_opcode = qspi::ReadOpcode::READ4O;
+    config.write_opcode = qspi::WriteOpcode::PP4O;
+    config.write_page_size = qspi::WritePageSize::_256BYTES;
+    config.frequency = qspi::Frequency::M16;
+    config.address_mode = qspi::AddressMode::_24BIT;
+
+    let qspi_irq = interrupt::take!(QSPI);
+    let mut qspi: qspi::Qspi<_, 67108864> = qspi::Qspi::new(
+        p.QSPI, qspi_irq, 
+        p.P0_19, p.P0_23, //sck, cs
+        p.P0_21, p.P0_22, p.P1_00, p.P0_17, //io 0,1,2,3
+        config
+    );
+
+    let mut mid = [1 ; 3];
+    qspi.blocking_custom_instruction(0x9F, &[], &mut mid).unwrap();
+    if mid[0] != 0xc8 || mid[1] != 0x40 || mid[2] != 0x15 {
+        panic!("Flash ID is incorrect!");
+    }
+
+
+    let mut numbuf = [0; 20];
+    let mut buf = AlignedBuf([0u8; FLASH_PAGE_SIZE]);
+
+    let pattern = |a: u32| (a ^ (a >> 8) ^ (a >> 16) ^ (a >> 24)) as u8;
+
+    for i in 0..8 {
+        uart.blocking_write(b"page erasing... ");
+        uart.blocking_write(i.numtoa(10, &mut numbuf));
+        uart.blocking_write(b"\r\n");
+        qspi.erase(i * FLASH_PAGE_SIZE).await.unwrap();
+
+        for j in 0..FLASH_PAGE_SIZE {
+            buf.0[j] = pattern((j + i * FLASH_PAGE_SIZE) as u32);
+        }
+
+        uart.blocking_write(b"programming...\r\n");
+        qspi.write(i * FLASH_PAGE_SIZE, &buf.0).await.unwrap();
+    }
+
+    for i in 0..8 {
+        uart.blocking_write(b"page  reading...");
+        uart.blocking_write(i.numtoa(10, &mut numbuf));
+        uart.blocking_write(b"\r\n");
+        qspi.read(i * FLASH_PAGE_SIZE, &mut buf.0).await.unwrap();
+
+        uart.blocking_write(b"verifying...\r\n");
+        for j in 0..FLASH_PAGE_SIZE {
+            assert_eq!(buf.0[j], pattern((j + i * FLASH_PAGE_SIZE) as u32));
+        }
+    }
+
+  
+
+
+    // qspi.blocking_erase(0).unwrap();
+
+    // let mut buf = AlignedBuf([0u8; FLASH_PAGE_SIZE]);
+    // buf.0[0] = 1;
+    // buf.0[1] = 2;
+    // qspi.blocking_write(0, &buf.0).unwrap();
+    // qspi.blocking_read(0, &mut buf.0).unwrap();
+
+    // let mut numbuf = [0; 20];
+    // for i in 0..3 {
+    //     uart.blocking_write(buf.0[i].numtoa(16, &mut numbuf)).unwrap();
+    // }
+
 
     redled.set_low();
     
