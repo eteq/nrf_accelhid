0e5bcaf887950eb5a1f11b6128dc7bfbd45db232
Erik Tollerud <erik.tollerud@gmail.com>
2023-03-09 23:34:28 -0500

    mpu6050 firmware mostly there but bank-writing not correct

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,34 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
+use cortex_m::prelude::_embedded_hal_blocking_delay_DelayMs;
 use embassy_executor::Spawner;
-use embassy_time::{Timer, Instant, Duration};
+use embassy_time::{Timer, Delay, Instant, Duration};
 
 use embassy_sync::signal::Signal;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 
 use embassy_nrf;
 use embassy_nrf::gpio::*;
-use embassy_nrf::{interrupt, uarte, config, qspi, peripherals};
+use embassy_nrf::{bind_interrupts, interrupt, uarte, config, qspi, peripherals, twim};
 
 use numtoa::NumToA;
 
 use panic_persist;
 
+mod dmp_firmware;
+
+
+bind_interrupts!(struct Irqs {
+    SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => twim::InterruptHandler<peripherals::TWISPI0>;
+    UARTE0_UART0 => uarte::InterruptHandler<peripherals::UARTE0>;
+    QSPI => qspi::InterruptHandler<peripherals::QSPI>;
+});
+
 const FLASH_PAGE_SIZE: usize = 256;
 // Workaround for alignment requirements in flash writing
 #[repr(C, align(4))]
 struct AlignedPageBuf([u8; FLASH_PAGE_SIZE]);
-
 
 static BUTTON_SIGNAL: Signal<CriticalSectionRawMutex, u32> = Signal::new();
 static DUMP_SIGNAL: Signal<CriticalSectionRawMutex, u32> = Signal::new();
@@ -47,8 +56,7 @@ async fn main(spawner: Spawner) {
     let mut uart_config = uarte::Config::default();
     uart_config.parity = uarte::Parity::EXCLUDED;
     uart_config.baudrate = uarte::Baudrate::BAUD115200;
-    let uart_irq = interrupt::take!(UARTE0_UART0);
-    let mut uart = uarte::Uarte::new(p.UARTE0, uart_irq, p.P0_25, p.P0_24, uart_config);
+    let mut uart = uarte::Uarte::new(p.UARTE0, Irqs, p.P0_25, p.P0_24, uart_config);
 
     if let Some(msg) = panic_persist::get_panic_message_bytes() {
         uart.write(b"Panic message on boot!:\r\n").await.unwrap();
@@ -64,15 +72,15 @@ async fn main(spawner: Spawner) {
     set_dotstar_color(0, 0, 0, 0, &mut dotstar_dat, &mut dotstar_clk);
 
     let mut config = qspi::Config::default();
+    config.capacity = 2 * 1024 * 1024; // 2 MB
     config.read_opcode = qspi::ReadOpcode::READ4O;
     config.write_opcode = qspi::WriteOpcode::PP4O;
     config.write_page_size = qspi::WritePageSize::_256BYTES;
     config.frequency = qspi::Frequency::M16;
     config.address_mode = qspi::AddressMode::_24BIT;
 
-    let qspi_irq = interrupt::take!(QSPI);
-    let mut qspi: qspi::Qspi<_, 67108864> = qspi::Qspi::new(
-        p.QSPI, qspi_irq, 
+    let mut qspi = qspi::Qspi::new(
+        p.QSPI, Irqs, 
         p.P0_19, p.P0_23, //sck, cs
         p.P0_21, p.P0_22, p.P1_00, p.P0_17, //io 0,1,2,3
         config
@@ -84,18 +92,14 @@ async fn main(spawner: Spawner) {
         panic!("Flash ID is incorrect!");
     }
 
-    // example for writing code
-    // let mut buf = AlignedPageBuf([0u8; FLASH_PAGE_SIZE]);
-    // qspi.blocking_erase(0);
-    // qspi.blocking_write(0, &buf.0);
-    // let mut rbuf = [1 ; 4];
-    // qspi.blocking_read(0, &mut rbuf).unwrap();
 
-    // Goal interface: 
-    // * dotstar starts off, redled on until init finishes
-    // * dotstar green while waiting for input
-    // * press button: erase (dotar red), record data (dotstar blue), next button press signals stop recording (dotstar back to green)
-    // * UART receives "d" : ignore if recording, otherwise dump flash (dotstar yellow)
+    let mut twim_config = twim::Config::default();
+    twim_config.frequency = twim::Frequency::K400;
+    let mut twim0 = twim::Twim::new(p.TWISPI0, Irqs, p.P0_16, p.P0_14, twim_config);
+    mpu6050_setup(&mut twim0, 0x68);
+    mpu6050_setup(&mut twim0, 0x69);
+
+    mpu6050_test_panic(&mut twim0, 0x68, false);
 
     let button = Input::new(p.P0_29.degrade(), Pull::Up);
     BUTTON_SIGNAL.reset();
@@ -105,6 +109,13 @@ async fn main(spawner: Spawner) {
     spawner.spawn(read_dump(rx_uart)).unwrap();
 
     redled.set_low();
+
+    
+    // Goal interface: 
+    // * dotstar starts off, redled on until init finishes
+    // * dotstar green while waiting for input
+    // * press button: erase (dotar red), record data (dotstar blue), next button press signals stop recording (dotstar back to green)
+    // * UART receives "d" : ignore if recording, otherwise dump flash (dotstar yellow)
 
     loop {
         set_dotstar_color(0, 255, 0, 5, &mut dotstar_dat, &mut dotstar_clk); //green -> waiting for input
@@ -116,10 +127,10 @@ async fn main(spawner: Spawner) {
             let mut flash_data = [0u8; 4];
 
             qspi.blocking_read(0, &mut flash_data).unwrap();
-            let ndata = usize::from_ne_bytes(flash_data);
+            let ndata = u32::from_ne_bytes(flash_data);
 
             for i in 0..ndata {
-                qspi.read(FLASH_PAGE_SIZE + i*4, &mut flash_data).await.unwrap();
+                qspi.read(i*4 + FLASH_PAGE_SIZE as u32, &mut flash_data).await.unwrap();
                 let towrite_bytes = u32::from_ne_bytes(flash_data).numtoa(10, &mut numtoa_buffer);
                 tx_uart.blocking_write(towrite_bytes).unwrap();
                 tx_uart.blocking_write(b"\r\n").unwrap();
@@ -155,9 +166,10 @@ async fn main(spawner: Spawner) {
 
             // start recording
             // TODO
+            let ndata = 234u32;
 
             // record length
-            let lenbuf: [u8; 4] = 234_u32.to_ne_bytes();  // note this is a count of *bytes* not bits
+            let lenbuf: [u8; 4] = ndata.to_ne_bytes();  // note this is a count of *bytes* not bits
             qspi.blocking_write(0, &lenbuf).unwrap(); // first page is always dedicated to just the length
 
             set_dotstar_color(0, 0, 255, 5, &mut dotstar_dat, &mut dotstar_clk); 
@@ -222,3 +234,116 @@ fn set_dotstar_color(rbyte:u8, gbyte:u8, bbyte:u8, brightness:u8,
     }
     clk.set_low();
 }
+
+fn mpu6050_setup<T: twim::Instance>(twim: &mut twim::Twim<T>, address: u8) {
+    let mut reg_buffer = [0; 1];
+    twim.blocking_write_read(address, &[0x75], &mut reg_buffer).unwrap();
+    if reg_buffer[0] != 0x68 { panic!("no/invalid response from MPU6050! {}", reg_buffer[0]); }
+
+    // device reset
+    twim.blocking_write(address, &[107, 0b10000000]).unwrap();
+    Delay.delay_ms(100u32);
+    // singal path reset - not clear if this is necessary but maybe recommended in register map doc?
+    twim.blocking_write(address, &[104, 0b00000111]).unwrap();
+    Delay.delay_ms(100u32);
+
+    twim.blocking_write(address, &[56,  0b00000000]).unwrap(); // no interrupts
+    twim.blocking_write(address, &[35,  0b00000000]).unwrap(); // Turn off regular FIFO to use DMP FIFO instead
+    twim.blocking_write(address, &[28,  0b00000000]).unwrap(); // 2g full scale accelerometer
+    twim.blocking_write(address, &[55,  0b10010000]).unwrap(); //logic level for int pin low, and clear int status on any read MAY NOT BE NEEDED?
+    twim.blocking_write(address, &[107, 0b00000001]).unwrap(); // Clock to PLL with X axis gyroscope reference, dont sleep
+    twim.blocking_write(address, &[26,  0b00000001]).unwrap(); // no external synchronization, set the DLPF to 184/188 Hz bandwidth
+    twim.blocking_write(address, &[25,  0b00000100]).unwrap(); // divides the sample rate - yields 200hz I think combined with above?
+	
+    mpu6050_write_firmware(twim, address, true);
+    // set DMP Program Start Address 0x0400
+    twim.blocking_write(address, &[dmp_firmware::MPU6050_REGADDR_DMP_PROG_START, 0x04, 0x00]).unwrap(); 
+   
+    twim.blocking_write(address, &[27,  0b00011000]).unwrap(); // set gyro to +2000 Deg/sec full range
+    twim.blocking_write(address, &[106, 0b11001100]).unwrap(); // turn on the fifo and reset it, and also turn on and reset the DMP (bits 7,3, respectively)
+    twim.blocking_write(address, &[56,  0b00000010]).unwrap(); // now turn on the RAW_DMP_INT_EN since the DMP is now set up
+  
+    twim.blocking_write_read(address, &[0x75], &mut reg_buffer).unwrap();
+    if reg_buffer[0] != 0x68 { panic!("no/invalid response from MPU6050 after reset/setup! {}", reg_buffer[0]); }
+}
+
+fn mpu6050_write_firmware<T: twim::Instance>(twim: &mut twim::Twim<T>, address: u8, verify: bool) {
+    // set the bank to 0
+    twim.blocking_write(address, &[dmp_firmware::MPU6050_REGADDR_BANK_SEL, 0]).unwrap();
+    // and the start address
+    twim.blocking_write(address, &[dmp_firmware::MPU6050_REGADDR_MEM_START_ADDR, 0]).unwrap();
+
+    let mut i = 0;
+    let mut write_buffer = [dmp_firmware::MPU6050_REGADDR_MEM_R_W; dmp_firmware::MPU6050_DMP_MEMORY_CHUNK_SIZE+1];
+    while i < dmp_firmware::DMP_FIRMWARE.len() {
+        let mut chunk_size = dmp_firmware::MPU6050_DMP_MEMORY_CHUNK_SIZE;
+
+        // write the last chunk without extra bytes
+        if (i + chunk_size) > dmp_firmware::DMP_FIRMWARE.len() { chunk_size = dmp_firmware::DMP_FIRMWARE.len() - i; }
+
+        for j in 0..chunk_size { write_buffer[j+1] = dmp_firmware::DMP_FIRMWARE[i+j]; }
+        twim.blocking_write(address, &write_buffer[0..(chunk_size+1)]).unwrap();
+        
+        i += chunk_size;
+    }
+
+    if verify {
+        //reset to bank and start address of 0 
+        twim.blocking_write(address, &[dmp_firmware::MPU6050_REGADDR_BANK_SEL, 0]).unwrap();
+        twim.blocking_write(address, &[dmp_firmware::MPU6050_REGADDR_MEM_START_ADDR, 0]).unwrap();
+        
+        let mut verify_buffer = [0; dmp_firmware::MPU6050_DMP_MEMORY_CHUNK_SIZE];
+        i = 0;
+        while i < dmp_firmware::DMP_FIRMWARE.len() {
+            twim.blocking_write_read(address, &[dmp_firmware::MPU6050_REGADDR_MEM_R_W],  
+                                    &mut verify_buffer).unwrap();
+
+            for j in 0..verify_buffer.len() {
+                if (i+j) < dmp_firmware::DMP_FIRMWARE.len() {
+                    if dmp_firmware::DMP_FIRMWARE[i+j] != verify_buffer[j] {
+                        panic!("verification of firmware failed at {}! {} vs {}", i + j, dmp_firmware::DMP_FIRMWARE[i+j], verify_buffer[j]);
+                    }
+                }
+            }
+            i += verify_buffer.len();
+
+        }
+    }
+}
+
+fn mpu6050_reset_fifo<T: twim::Instance>(twim: &mut twim::Twim<T>, address: u8) {
+    let mut reg_buffer = [0; 1];
+    // first read the USER_CTRL register
+    twim.blocking_write_read(address, &[106], &mut reg_buffer).unwrap();
+    twim.blocking_write(address, &[106, reg_buffer[0] | 0b00000100]).unwrap();  // write back the same thing but with the fifo reset bit set
+}
+
+fn mpu6050_get_fifo_count<T: twim::Instance>(twim: &mut twim::Twim<T>, address: u8) -> u16 {
+    let mut rd_buffer = [0; 2];
+    // first read the USER_CTRL register
+    twim.blocking_write_read(address, &[114], &mut rd_buffer).unwrap();
+    ((rd_buffer[0] as u16) << 8) | (rd_buffer[1] as u16)
+}
+
+// entirely for testing that it actually produces a good fifo output
+fn mpu6050_test_panic<T: twim::Instance>(twim: &mut twim::Twim<T>, address: u8, basic_fifo: bool) {
+    
+    if basic_fifo {
+        twim.blocking_write(address, &[35,  0b01111000]).unwrap(); //regular fifo on for accel/gyro
+    }
+
+    mpu6050_reset_fifo(twim, address);
+    let mut count = mpu6050_get_fifo_count(twim, address);
+    let mut nzeros: usize = 0;
+    while count == 0 {
+        count = mpu6050_get_fifo_count(twim, address);
+        //panic!("in 0 count");
+        nzeros += 1;
+    }
+
+    panic!("reached non-zero count {}, after {} zeros", count, nzeros);
+    
+    //end test
+}
+
+
